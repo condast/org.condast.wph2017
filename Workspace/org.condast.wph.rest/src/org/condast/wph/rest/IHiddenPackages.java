19c51f5eb729d4c7daf68447f36d91cf9edc76ef
Kees <info@condast.com>
2017-08-19 15:09:08 +0200

    Changes to get the rest API t work with Glassfish Jersey 2.25

--- Workspace/org.condast.wph.rest/src/org/condast/wph/rest/IHiddenPackages.java
+++ Workspace/org.condast.wph.rest/src/org/condast/wph/rest/IHiddenPackages.java
@@ -2,7 +2,6 @@ package org.condast.wph.rest;
 
 import org.osgi.framework.*;
 import org.glassfish.jersey.*;
-import org.glassfish.jersey.inject.hk2.*;
 
 @SuppressWarnings("unused")
 public interface IHiddenPackages {
